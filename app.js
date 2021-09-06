5840879599481216eba17b5c6de3a55e1e3ca585
Dickson <dickson.njuguna@student.moringaschool.com>
2021-09-06 11:38:42 +0300

    add constants

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
 
-
-
-
-const from =document.querySelector("#frmGhana");
-const mNames = ["kwasi","kwadwo","kwabena","kwaku","yaw","kofi","kwame"]
-const fNames =["akoua","Adwoa","abenaa","akua","yaa","afua","ama"]
-
-
-
-
-
 const akanName = (e) => {
     e.preventDefault();
-    const frm = document.querySelector("frmGhana");
+    const mNames = ["kwasi","kwadwo","kwabena","kwaku","yaw","kofi","kwame"]
+    const fNames =["akoua","Adwoa","abenaa","akua","yaa","afua","ama"]
+    const frm = document.querySelector("#frmGhana");
     const fd = new FormData(frm);
+    const gender = fd.get("gender");
+    const  dateGhana =document.getElementById("date").value;
+    const  monthGhana =document.getElementById("month").value;
+    const  yearGhana =document.getElementById("year").value;
+    const d =new Date(yearGhana, (monthGhana-1),dateGhana).getDay();
 
-    console.log(fd.get("gender"));
-};
+
+    
+
+    console.log(fd.get("date of birth"));
+}
+   
+
 document.querySelector("#frmGhana").addEventListener('submit', akanName);
 
 
