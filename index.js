eee8224e71f34872a379d537685824bacc1afca0
Dickson <dickson.njuguna@student.moringaschool.com>
2021-09-06 09:18:00 +0300

    add content

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
+const from =documet.querySelector("form")
+const mNames = ["kwasi","kwadwo","kwabena","kwaku","yaw","kofi","kwame"]
+const fNames =["akoua","Adwoa","abenaa","akua","yaa","afua","ama"]
 
-const akanName = (e) => {
+
+
+
+
+
+
+const akanName = () => {
     e.preventDefault();
-    const frm = document.querySelector("#frmGhana");
+    const frm = document.querySelector("frmGhana");
     const fd = new FormData(frm);
 
     console.log(fd.get("gender"));
-}
-
-
-document.querySelector("frmGhana").addEventlistener('submit', akanName);
+};
+document.querySelector("frmGhana").addEventListener(submit, akanName);
